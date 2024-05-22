025ba297577089edb41ebc9155787f8c7906ccfe
matthiasdotsh <git@matthias.sh>
2024-05-22 21:06:45 +0200

    Experiment with overrideAttrs

--- pkgs/development/python-modules/soundcard/shell.nix
+++ pkgs/development/python-modules/soundcard/shell.nix
@@ -1,7 +1,20 @@
-with (import /home/ms/git/nixpkgs {});
-mkShell {
+{ pkgs ? import ../../../.. { } }:
+
+let
+  # Custom version of the SoundCard package
+  custom_soundcard = pkgs.python3Packages.soundcard.overrideAttrs (oldAttrs: rec {
+    version = "0.4.0";
+    src = pkgs.python3Packages.fetchPypi {
+      pname = oldAttrs.pname;
+      version = version;
+      hash = "sha256-fYxPQZm4jugjEwLptbgT6iOk+pVY3MY5bDIBF7BBtZ4=";
+    };
+  });
+
+in
+pkgs.mkShell {
   buildInputs = [
-    (python3.withPackages (p: [ p.soundcard ]))
+    # (pkgs.python3.withPackages (p: [ custom_soundcard ]))
+    (pkgs.python3.withPackages (p: [ p.soundcard ]))
   ];
 }
-
